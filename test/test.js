83b874b6432f0d3e2811290d529013fa6a8818a8
Thanawat <thanawatk65@nu.ac.th>
2026-03-13 12:47:31 +0700

    change test

--- test/test.js
+++ test/test.js
@@ -1,15 +1,15 @@
-const request = require("supertest")
-const app = require("../app")
+const request = require("supertest");
+const app = require("../app");
 
-describe("API Test",()=>{
+describe("API Test", () => {
+    // ถ้ายังมีปัญหาค้าง ให้ใช้ท่านี้เพื่อบังคับปิด connection หลังเทสต์เสร็จ
+    afterAll(async () => {
+        // ในกรณีที่มี database connection ให้ close ตรงนี้ด้วยครับ
+    });
 
-    it("GET / should return message", async()=>{
-
-        const res = await request(app).get("/")
-
-        expect(res.statusCode).toBe(200)
-        expect(res.body.message).toBe("DevSecOps Mini Project Running")
-
-    })
-
-})
+    it("GET / should return message", async () => {
+        const res = await request(app).get("/");
+        expect(res.statusCode).toBe(200);
+        expect(res.body.message).toBe("DevSecOps Mini Project Running");
+    });
+});
